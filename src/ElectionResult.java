1a2b181ca5d1f0fe546a0e8e7068dffb56180c71
arielfreiman <afreiman938@student.fuhsd.org>
2019-03-10 13:28:38 -0700

    Create class ElectionResult and create fields

---
+++ src/ElectionResult.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ElectionResult {
+    private double votes_dem;
+    private double votes_gop;
+    private double total_votes;
+    private double per_dem;
+    private double per_gop;
+    private double diff;
+    private double per_point;
+    private double per_point_diff;
+    private String state_abbr;
+    private String country_name;
+    private String combined_fips;
+
+}
